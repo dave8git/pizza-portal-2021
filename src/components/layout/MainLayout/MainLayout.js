fd6c69145561a6aa47f98bf1cd0168b07d4eb13e
unknown <dave8git@gmail.com>
2022-03-22 17:06:37 -0700

    add material-ui --not-working #1

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -2,10 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 //import styles from './MyComponent.scss';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 
 const MainLayout = (props) => (
-  <div>
-    <PageNav />
+  <div className="MainLayout">
+    <AppBar>
+      <Toolbar>
+        <PageNav />
+      </Toolbar>
+    </AppBar>
+    
     {props.children}
   </div>
 );
